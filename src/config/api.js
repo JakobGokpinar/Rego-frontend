afdc8e074f31e0f7c78ad6df2d65cdc05355d38e
AhmetGkp <ahmettabar2003@gmail.com>
2024-04-26 18:03:51 +0200

    email validation, uri links update

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
-// https://rego-api.onrender.com
-//http://localhost:3080/
 
-const serverURL = "http://localhost:3080/";
+const serverURL = "https://rego-api.onrender.com";   //http://localhost:3080/              https://rego-api.onrender.com
 const userApi = "user";
 const fileApi = "file";
 
--- src/config/socket.js
+++ src/config/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 // https://rego-api.onrender.com
 //http://localhost:3080/
 
-let serverLink = 'http://localhost:3080/';
+let serverLink = 'https://rego-api.onrender.com';
 const socket = io(serverLink)
 
 export default socket
